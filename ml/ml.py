85e5d0c168be8118c30e6d09b455781c941b09a9
alinagasparan <alinagasparan0908@gmail.com>
2026-03-31 17:04:56 +0500

    Refactor BERT model loading and user query handling

--- ml/ml.py
+++ ml/ml.py
@@ -2,21 +2,20 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# === 1. Загрузка модели BERT ===
-print("🚀 Загружаем модель BERT...")
-model = SentenceTransformer('all-MiniLM-L6-v2')  # компактная и быстрая
 
-# === 2. Загрузка базы фильмов ===
-df = pd.read_csv('../movies.csv')  # путь: на уровень выше, если база рядом с ml
+print("🚀 Загружаем модель BERT...")
+model = SentenceTransformer('all-MiniLM-L6-v2')  
+
+df = pd.read_csv('../movies.csv') 
 df = df.dropna(subset=['overview'])
 
-# === 3. Преобразуем описания в векторы ===
+
 print("🔹 Преобразуем описания фильмов в векторы...")
 movie_embeddings = model.encode(df['overview'].tolist(), show_progress_bar=True)
 
 print("✅ Всё готово! Вводи описание фильма:")
 
-# === 4. Запрос пользователя и поиск ===
+
 while True:
     query = input("\n🔎 Опиши фильм: ")
     
@@ -25,10 +24,10 @@ while True:
     
     query_vec = model.encode([query])
     
-    # cosine similarit
+    
     scores = cosine_similarity(query_vec, movie_embeddings)[0]
     
-    # топ-5 фильмов
+    
     top_indices = scores.argsort()[-5:][::-1]
     
     print("\n🎬 Топ-5 фильмов по смыслу:\n")
