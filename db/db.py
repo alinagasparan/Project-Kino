927bb4f1550b4a6149681c8fed5003d2fe9435b8
Hilalova-Malohat <malochat5@gmail.com>
2026-04-02 18:25:16 +0500

    Добавление в Планы + список Запланированных фильмов

--- db/db.py
+++ db/db.py
@@ -2,6 +2,7 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 #Подключение к серверу для работы с базой
@@ -32,5 +33,27 @@ def user_register(conn, user_name, user_password, avatar_url=None):
         user_id = cur.fetchone()[0]
     conn.commit()
     return user_id
+
+#Добавление фильма в планы
+def add_film_to_planned(conn, user_id, film_id):
+    with conn.cursor() as cur:
+        query = """INSERT INTO users_schema.user_movies (user_id, film_id, status) 
+        VALUES (%s, %s, 'planned')
+        ON CONFLICT (user_id, film_id) DO NOTHING
+        RETURNING user_id;"""
+        cur.execute(query, (user_id, film_id))
+        result = cur.fetchone()
+        conn.commit()
+        return bool(result)
+
+#Словарь из названий фильмов, которые в планах у пользователя
+def get_users_planned_films(conn, user_id):
+    with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        query = """SELECT f.film_id, f.title FROM users_schema.user_movies um
+        JOIN public.film f ON f.film_id = um.film_id
+        WHERE um.user_id = %s AND um.status = 'planned';"""
+        cur.execute(query, (user_id,))
+        films = cur.fetchall()
+    return films
 
 
--- db/main.py
+++ db/main.py
@@ -1,11 +1,15 @@
 import db
-from db import user_register
+from db import user_register, add_film_to_planned, get_users_planned_films
 
 conn = db.get_connection()
 cur = conn.cursor()
 
-#cur.execute("SELECT * FROM film LIMIT 5;")
+cur.execute("SELECT * FROM film LIMIT 5;")
+print(cur.fetchall())
 print(db.get_count(conn, "film"))
-
+cur.execute("SELECT title FROM public.film WHERE film_id=133")
+print(cur.fetchall())
+add_film_to_planned(conn, 2, 133)
+print(get_users_planned_films(conn, 2))
 cur.close()
 conn.close()
